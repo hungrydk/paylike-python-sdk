a42d9821f9e746b5057c982c9c74556049ddb2e4
Sune Kjærgård <sune@hungry.dk>
2017-05-23 12:55:41 +0200

    initial implementation
    Untested

---
+++ paylike/__init__.py
@@ -0,0 +1,74 @@
+import requests
+from decimal import Decimal
+
+
+class PaylikeApiClient:
+    api_key = None
+    merchant_id = None
+    api_base_url = "https://api.paylike.io"
+
+    def __init__(self, api_key="", merchant_id=""):
+        self.api_key = api_key
+        self.merchant_id = merchant_id
+
+    def cancel_transaction(self, transaction_id, amount):
+        return self._call_api('/transaction/%s/voids' % transaction_id,
+                              method='POST',
+                              data={
+                                  "amount": self._convert_to_paylike_amount(amount),
+                              })
+
+    def capture_transaction(self, transaction_id, amount, descriptor='', currency=None):
+        data = {
+            "amount": self._convert_to_paylike_amount(amount),
+            "descriptor": descriptor
+        }
+        if currency is not None:
+            data["currency"] = currency
+
+        return self._call_api('/transaction/%s/captures' % transaction_id,
+                              method='POST',
+                              data=data)
+
+    def create_payment_from_transaction(self, transaction_id, currency, amount, descriptior=''):
+        return self._call_api('/merchants/%s/transactions/', method='POST', data={
+            "transactionId": transaction_id,
+            "descriptor": descriptior,
+            "currency": currency,
+            "amount": self._convert_to_paylike_amount(amount)
+        })
+
+    def create_payment_from_saved_card(self, card_id, currency, amount, descriptior=''):
+        return self._call_api('/merchants/%s/transactions/', method='POST', data={
+            "cardId": card_id,
+            "descriptor": descriptior,
+            "currency": currency,
+            "amount": self._convert_to_paylike_amount(amount)
+        })
+
+    def get_transaction(self, transaction_id):
+        return self._call_api('/transaction/%s/' % transaction_id)
+
+    def get_transactions(self, limit=100):
+        return self._call_api('/merchants/%s/transactions/?limit=%i' % (self.merchant_id, limit))
+
+    def refund_transaction(self, transaction_id, amount, descriptor=""):
+        return self._call_api('/transaction/%s/refunds' % transaction_id,
+                              method='POST',
+                              data={
+                                  "amount": self._convert_to_paylike_amount(amount),
+                                  "descriptor": descriptor
+                              })
+
+    def _call_api(self, uri, method='GET', data={}, headers={}):
+
+        url = "%s%s" % (self.api_base_url, uri)
+        auth = ("",self.api_key) if self.api_key is not None else None
+
+        r = requests.request(method, url, json=data, headers=headers, params=data, auth=auth)
+
+        return r.json()
+
+    def _convert_to_paylike_amount(amount):
+        assert isinstance(amount, Decimal)
+        return int(round(amount * 100))
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='paylike',
+      version='0.1',
+      description='Python wrapper for paylike.io\'s REST API',
+      url='https://github.com/hungrydk/paylike-python-sdk',
+      author='Sune Kjaergaard',
+      author_email='sune@hungry.dk',
+      license='MIT',
+      packages=['paylike'],
+      zip_safe=False)
